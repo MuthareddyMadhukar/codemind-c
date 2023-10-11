a5544026ad5dbcaf7ec92c8a14e4053d2fb3d14c
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2023-10-11 10:43:45 +0530

    #include <stdio.h> #include <math.h> int main() {     int f,x,t;     scanf("%d",&x);     f=4;     t=f*x;     if (t<=1000)     printf("YES");     else     printf("NO");
    }

---
+++ Tickets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int f,x,t;
+    scanf("%d",&x);
+    f=4;
+    t=f*x;
+    if (t<=1000)
+    printf("YES");
+    else
+    printf("NO");
+    
+}
