96f89aede35e30faa10641e37ee07d30f9684d50
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2023-10-13 21:16:23 +0530

    #include <stdio.h> #include <math.h> int main() {     char color,V,I,B,G,Y,O,R;     scanf("%c",&color);     if (color==V)     printf("Violet");     else if (color=='I')     printf("Indigo");     else if (color=='B')     printf("Blue");     else if (color=='G')     printf("Green");     else if (color=='Y')     printf("Yellow");     else if (color=='O')     printf("Orange");     else     printf("-1");
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    char color,V,I,B,G,Y,O,R;
+    scanf("%c",&color);
+    if (color==V)
+    printf("Violet");
+    else if (color=='I')
+    printf("Indigo");
+    else if (color=='B')
+    printf("Blue");
+    else if (color=='G')
+    printf("Green");
+    else if (color=='Y')
+    printf("Yellow");
+    else if (color=='O')
+    printf("Orange");
+    else
+    printf("-1");
+    
+}
