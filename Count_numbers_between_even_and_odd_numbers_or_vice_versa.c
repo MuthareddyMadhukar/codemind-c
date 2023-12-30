f735c435a783d635d32c67fa0ddd992759f3564a
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2023-12-30 12:06:52 +0530

    #include <stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);     int arr[n];     for (i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for (i=1;i<n-1;i++)     {         if (arr[i-1]%2==0 && arr[i+1]%2==1 || arr[i-1]%2==1 && arr[i+1]%2==0)         count++;     }     printf("%d",count);
    }

---
+++ Count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (i=1;i<n-1;i++)
+    {
+        if (arr[i-1]%2==0 && arr[i+1]%2==1 || arr[i-1]%2==1 && arr[i+1]%2==0)
+        count++;
+    }
+    printf("%d",count);
+
+}
