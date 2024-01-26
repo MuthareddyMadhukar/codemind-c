943d0d2409b4bcd154b0712c01564b929194b7a7
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2024-01-26 12:03:55 +0530

    #include <stdio.h> #include <ctype.h> #include <string.h> int main() {     int i,count=0;     char s[20];     scanf("%[^ ]c",&s);     for (i=0;s[i]!=NULL;i++)     {        if (s[i]==' '||s[i]==' ')        count++;     }     count++;     printf("%d",count); }

---
+++ Count_words_in_String.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+    int i,count=0;
+    char s[20];
+    scanf("%[^
+]c",&s);
+    for (i=0;s[i]!=NULL;i++)
+    {
+       if (s[i]==' '||s[i]=='
+')
+       count++;
+    }
+    count++;
+    printf("%d",count);
+}
